73c61491ca68c061190f97476cee6dd4f7dd71dc
vivekanandd1 <vivekanandd1@live.com>
2022-06-11 13:54:25 +0530

    new db

--- src/MySQLDemos/MySQL2ndDemo.java
+++ src/MySQLDemos/MySQL2ndDemo.java
@@ -10,19 +10,19 @@ public class MySQL2ndDemo {
 	
 	public static void main(String[] args) throws SQLException {
 		
-		Connection cnnct = DriverManager.getConnection("jdbc:mysql://localhost:3306/jointac","root","root");
+		Connection cnnct = DriverManager.getConnection("jdbc:mysql://localhost:3306/new_db","root","root");
 		
 		Statement stmnt = cnnct.createStatement();
 		
-		String s = "SELECT CUSTNAME,BALANCE,ACTYPE FROM acdetails";
+		String s = "SELECT country_id,country_name,region_id FROM countries";
 		
 		ResultSet rs = stmnt.executeQuery(s);
 		
 		while(rs.next())
 		{
-			String custname =rs.getString("CUSTNAME");
-			int balance = rs.getInt("BALANCE");
-			String actype = rs.getString("ACTYPE");
+			String custname =rs.getString("country_id");
+			int balance = rs.getInt("region_id");
+			String actype = rs.getString("country_name");
 			
 			System.out.println(custname+"    "+balance+"     "+actype);
 		}
